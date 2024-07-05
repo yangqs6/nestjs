5c41cced25177d6fd96cf183312ef81aaad58571
yangqs6 <87424645+yangqs6@users.noreply.github.com>
2024-07-05 16:08:26 -0500

    add guard to filter access

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UserController {
 
+    @UseGuards(AuthGuard("jwt"))
     @Get("me")
     getMe(){
-        return "user info"
+        return "user info11"
     }
 }
